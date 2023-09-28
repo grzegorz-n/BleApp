6177220817482c8a86989b1b73c83cb1b6ed8de5
grzegorz-n <naczynskie.grzegorz@gmail.com>
2023-09-28 15:01:13 +0200

    v28.09

--- app/src/main/java/com/example/bleapp/MainActivity.kt
+++ app/src/main/java/com/example/bleapp/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.example.bleapp
 
 import android.Manifest
+import android.annotation.SuppressLint
 import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothDevice
 import android.bluetooth.BluetoothManager
+import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.content.IntentFilter
 import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -28,21 +32,60 @@ class MainActivity : AppCompatActivity() {
             } else {
                 // jest ble
                 if (ble.isEnabled) {
-                    Toast.makeText(getApplicationContext(), "ble jest włączone", Toast.LENGTH_LONG).show()
-                    val enableIntent: Intent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
-                    if (ContextCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_CONNECT) == PackageManager.PERMISSION_GRANTED) {
-                        Toast.makeText(getApplicationContext(), "Mam uprawnienia", Toast.LENGTH_LONG).show()
-                    } else {
-                        Toast.makeText(getApplicationContext(), "Nie mam uprawnien", Toast.LENGTH_LONG).show()
-                        startActivity(enableIntent)
+
+                    //tworzę tablicę uprwanień
+                    val myPermissions: Array<String> = arrayOf(android.Manifest.permission.BLUETOOTH,
+                        android.Manifest.permission.BLUETOOTH_ADMIN,
+                        android.Manifest.permission.BLUETOOTH_CONNECT,
+                        android.Manifest.permission.BLUETOOTH_SCAN,
+                        android.Manifest.permission.ACCESS_FINE_LOCATION,
+                        android.Manifest.permission.ACCESS_COARSE_LOCATION)
+
+                    //tworzę zmienną która informuje czy mam uprawnienia
+                    var doPremissions = myPermissions.all {
+                        (ContextCompat.checkSelfPermission(this, it) == PackageManager.PERMISSION_GRANTED)
                     }
 
+
+                    if (doPremissions) {
+                        //tu mam uprawnienia
+                        Toast.makeText(getApplicationContext(), "Mam uprawnienia", Toast.LENGTH_LONG).show()
+                    } else {
+                        // a tu się pytam o uprawnienia
+                        requestPermissions(myPermissions, 1)
+                    }
+                    scanFunction(ble)
+                    ble?.startDiscovery()
+
                 } else {
+                    // tutaj ble jest w ogóle wyłączone
                     Toast.makeText(getApplicationContext(), "ble jest wyłączone", Toast.LENGTH_LONG).show()
+                }
+            }
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun scanFunction(ble: BluetoothAdapter): Unit {
+        val deviceList: MutableList<BluetoothDevice> = mutableListOf<BluetoothDevice>()
+        val receiver = object: BroadcastReceiver() {
+            override fun onReceive(context: Context?, intent: Intent?) {
+                when(intent?.action) {
+                    BluetoothDevice.ACTION_FOUND -> {
+                        val device = intent.getParcelableExtra<BluetoothDevice>(BluetoothDevice.EXTRA_DEVICE)
+                        device?.let {
+                            deviceList.add(it)
+                        }
+                    }
                 }
 
             }
         }
+        val filter = IntentFilter(BluetoothDevice.ACTION_FOUND)
+        registerReceiver(receiver, filter)
+
+
+
 
     }
 }
